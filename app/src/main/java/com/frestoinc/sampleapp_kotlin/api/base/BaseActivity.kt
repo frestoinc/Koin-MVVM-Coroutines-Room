66dda175cf2a6147d9b1d69f642e7d0053335cfa
r00t <aliffridzuan88@gmail.com>
2020-02-27 15:20:42 +0800

    -BaseActivity :implement databinding and viewmodel holder

--- app/src/main/java/com/frestoinc/sampleapp_kotlin/api/base/BaseActivity.kt
+++ app/src/main/java/com/frestoinc/sampleapp_kotlin/api/base/BaseActivity.kt
@@ -1,9 +1,41 @@
 package com.frestoinc.sampleapp_kotlin.api.base
 
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 /**
  * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
  */
 
-abstract class BaseActivity<T : ViewDataBinding, V : BaseViewModel>
+abstract class BaseActivity<T : ViewDataBinding, V : BaseViewModel> : AppCompatActivity() {
+
+    lateinit var viewDatabinding: T
+
+    lateinit var viewModel: V
+
+    @get:LayoutRes
+    protected abstract var getLayoutId: Int
+
+    protected abstract var getViewModel: V
+
+    protected abstract var getBindingVariable: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setDataBinding()
+    }
+
+    private fun setDataBinding() {
+        viewDatabinding = DataBindingUtil.setContentView(this, getLayoutId)
+        this.viewModel = getViewModel
+        viewDatabinding.setVariable(getBindingVariable, viewModel)
+        viewDatabinding.executePendingBindings()
+    }
+
+    fun getViewDataBinding(): T {
+        return viewDatabinding
+    }
+}
--- app/src/main/java/com/frestoinc/sampleapp_kotlin/ui/MainActivity.kt
+++ app/src/main/java/com/frestoinc/sampleapp_kotlin/ui/MainActivity.kt
@@ -6,4 +6,16 @@ import com.frestoinc.sampleapp_kotlin.databinding.ActivityMainBinding
 /**
  * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
  */
-class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>()
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+
+    override var getLayoutId: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        set(value) {}
+    override var getViewModel: MainViewModel
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        set(value) {}
+    override var getBindingVariable: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        set(value) {}
+
+}

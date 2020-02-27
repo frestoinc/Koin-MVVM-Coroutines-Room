26c828c93013d7565f2593b6c2a7c66099f6e7ad
r00t <aliffridzuan88@gmail.com>
2020-02-27 12:28:32 +0800

    -removed dagger and replaced with koin -init appmodule

---
+++ app/src/main/java/com/frestoinc/BaseApplication.kt
@@ -0,0 +1,28 @@
+package com.frestoinc
+
+import android.app.Application
+import com.frestoinc.sampleapp_kotlin.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+/**
+ * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
+ */
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@BaseApplication)
+            modules(appModule)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/frestoinc/sampleapp_kotlin/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.frestoinc.sampleapp_kotlin.di
+
+import org.koin.dsl.module
+
+/**
+ * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
+ */
+
+val appModule = module {
+
+
+}

fdc5f32fbba1f73aac146ab6685112d955949b34
r00t <aliffridzuan88@gmail.com>
2020-02-27 13:06:22 +0800

    -added base for viewholder

---
+++ app/src/main/java/com/frestoinc/api/base/BaseViewHolder.kt
@@ -0,0 +1,10 @@
+package com.frestoinc.api.base
+
+import android.view.View
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
+ */
+abstract class BaseViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView)
--- app/src/main/java/com/frestoinc/api/base/BaseViewModel.kt
+++ app/src/main/java/com/frestoinc/api/base/BaseViewModel.kt
@@ -5,5 +5,4 @@ import androidx.lifecycle.ViewModel
 /**
  * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
  */
-
 abstract class BaseViewModel : ViewModel()

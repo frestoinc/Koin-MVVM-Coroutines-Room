984d42e36acd5c98b3899be4975295df62aefe9e
r00t <aliffridzuan88@gmail.com>
2020-02-27 12:59:25 +0800

    -added lifecycle library -added BaseActivity -added BaseViewModel

---
+++ app/src/main/java/com/frestoinc/api/base/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.frestoinc.api.base
+
+import androidx.databinding.ViewDataBinding
+
+/**
+ * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
+ */
+
+abstract class BaseActivity<T : ViewDataBinding, V : BaseViewModel>
---
+++ app/src/main/java/com/frestoinc/api/base/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.frestoinc.api.base
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by frestoinc on 27,February,2020 for SampleApp_Kotlin.
+ */
+
+abstract class BaseViewModel : ViewModel()
